d73dc3ef8b877f1e500cd80cda2003a20014755b
Asnise_Git <55057145+asnise@users.noreply.github.com>
2023-03-18 08:14:51 +0700

    Add files via upload

--- server.js
+++ server.js
@@ -1,14 +1,16 @@
-const http = require('http');
+const express = require('express');
+const bodyParser = require('body-parser');
 
-const hostname = 'localhost';
+const app = express();
 const port = process.env.PORT || 3000;
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World\n');
+app.use(bodyParser.json());
+
+app.post('/messages', (req, res) => {
+  console.log(req.body);
+  res.send('Message received');
 });
 
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
 });
